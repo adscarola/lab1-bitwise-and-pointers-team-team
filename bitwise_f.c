037c8e550466bb0d1e67ff27a6e73d575fa1d813
Azaylea Martinez <azayleamartinez@Lenny.utep.edu>
2024-02-05 11:31:20 -0700

    pushing in c file

---
+++ bitwise_f.c
@@ -0,0 +1,21 @@
+#include "bitwise_f.h"
+
+void swap(short *n1, short *n2) { return; }
+
+int has_odd_number_of_bits_set(int n) { return -1; }
+
+int turn_k_bit_off(const int n, const int k) { return -1; }
+
+int k_bit_is_on(const int n, const int k) { return -1; }
+
+int toggle_k_bit(const int n, const int k) { return -1; }
+
+int is_palindrome(const int n) { return -1; }
+
+short copy_reverse_bits(short n) { return -1; }
+
+void print_number(const unsigned char number, const enum reflection refl) {
+  return;
+}
+
+int is_unique(const char *s) { return -1; }
